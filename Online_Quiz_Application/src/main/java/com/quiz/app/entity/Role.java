98bf0c8e464983d04a88c95d82646ea7874ba1dd
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-05-31 15:41:39 +0530

    intergrated frontend

---
+++ Online_Quiz_Application/src/main/java/com/quiz/app/entity/Role.java
@@ -0,0 +1,53 @@
+package com.quiz.app.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="role_name")
+	private String roleName;
+	
+	@ManyToMany(mappedBy = "roles")
+	private List<User> users;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+
+
+}
