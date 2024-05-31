3a41990ab6849a744f8249a58a557fef340a31f5
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-05-31 15:08:09 +0530

    created spring boot application

---
+++ Online_Quiz_Application/src/main/java/com/quiz/app/OnlineQuizApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizApplication.class, args);
+	}
+
+}
